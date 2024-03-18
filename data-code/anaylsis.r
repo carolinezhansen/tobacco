da3a5dfe63c2e6d71116207e4ea33bd2bc4738a9
Caroline Hansen <carolinezhansen@gmail.com>
2024-03-17 20:16:14 -0400

    1-7

--- data-code/anaylsis.r
+++ data-code/anaylsis.r
@@ -1,7 +1,7 @@
 # submission 1 analysis
 
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table, gdata, dplyr,tidyr)
+pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table, gdata, dplyr,tidyr, AER)
 
 source("data/output/TaxBurden_Data.rds")
 
@@ -72,13 +72,41 @@ ggplot(filtered_data2, aes(x = Year, y = sales_per_capita, group = state, color 
 # Problem 5
 # Compare the trends in sales from the 5 states with the highest price increases to those with the lowest price increases.
 
-ggplot() +
+compareincreases <- ggplot() +
   geom_line(data = filtered_data, aes(x = Year, y = sales_per_capita, color = state), linetype = "solid") +
   geom_line(data = filtered_data2, aes(x = Year, y = sales_per_capita, color = state), linetype = "dashed") +
   labs(x = "Year", y = "Average Packs Sold per Capita", 
        title = "Comparison of Sales Trends between High and Low Price Increase States",
        color = "State") +
-  scale_color_manual(values = c("red", "blue"), labels = c("High Price Increase", "Low Price Increase")) +
+  scale_color_viridis_d() + 
   theme_minimal()
-
+print(compareincreases)
 # problem 6
+# Focusing only on the time period from 1970 to 1990, regress log sales on log prices to estimate the price elasticity of demand over that period. Interpret your results.
+
+filtered_data3 <- final.data[final.data$Year >= 1970 & final.data$Year <= 1990, ]
+
+model <- lm(log(sales_per_capita) ~ log(cost_per_pack), data = filtered_data)
+
+reg1 <- summary(model)
+
+print(reg1)
+
+# the p-value is less than zero which means that there is a statistically significant relationship between sales and cost per pack. The coeficcient for cost per pack is -0.406 which means that there in an inelastic demand. So when 1% increase in cigarette price leds to a less than 1% decrease in sales per capita. 
+
+# problem 7
+# Again limiting to 1970 to 1990, regress log sales on log prices using the total (federal and state) cigarette tax (in dollars) as an instrument for log prices. Interpret your results and compare your estimates to those without an instrument. Are they different? If so, why?
+
+iv_model <- ivreg(log(sales_per_capita) ~ log(cost_per_pack) | log(tax_dollar), data = filtered_data3)
+
+# Display IV regression summary
+summary(iv_model)
+
+# problem 8
+# Show the first stage and reduced-form results from the instrument.
+
+# problem 9 
+# Repeat questions 1-3 focusing on the period from 1991 to 2015.
+
+# problem 10
+# Compare your elasticity estimates from 1970-1990 versus those from 1991-2015. Are they different? If so, why?
